7cb841763b0f1d868ca09de6267cf651270be319
bendzasky <bendzasky@gmail.com>
2014-12-08 18:06:34 +0100

    kernel update

--- src/invoker/kernel/kernel.cu
+++ src/invoker/kernel/kernel.cu
@@ -1,10 +1,15 @@
 
 #include "kernel.cuh"
 #include "gpuErrchk.cuh"
-
+#include "..\..\matrix_loader\matrix.hpp"
 #include <assert.h>
 #include <iostream>
 #include <stdio.h>
+
+__device__ int getGlobalIdx_1D_1D()
+{
+	return blockIdx.x *blockDim.x + threadIdx.x;
+}
 
 __device__ int getGlobalIdx_2D_2D()
 {
@@ -14,74 +19,128 @@ __device__ int getGlobalIdx_2D_2D()
 	return threadId;
 }
 
-__device__ float sumMatrix(const float * const d_inputMatrix1, const float * const d_inputMatrix2, int index)
+__device__ CellInfo sumMatrix(const CellInfo * const d_inputMatrix1, const CellInfo * const d_inputMatrix2, int index)
 {
-	return d_inputMatrix1[index] + d_inputMatrix2[index];
+	CellInfo result = {
+		d_inputMatrix1[index].value + d_inputMatrix2[index].value,
+		d_inputMatrix1[index].row,
+		d_inputMatrix1[index].column
+						};
+	return result;
 }
 
-__device__ void injectError(float * const d_inputMatrix, const float * const d_errorMap, int index)
+__device__ bool isError(int index)
 {
-	d_inputMatrix[index] += d_errorMap[index];
+	// what is the reason for error? 
+	return true;
 }
 
-__global__ void matrixOperation(const float  * const d_inputMatrix1,
-	const float  * const d_inputMatrix2, float * const d_outputMatrix)
+__device__ void injectError(CellInfo &inputCell)
 {
-	int index = getGlobalIdx_2D_2D();
+	inputCell.value += 99999999;
+}
+
+__global__ void kernel(const CellInfo  * const d_inputMatrix1,
+	const CellInfo  * const d_inputMatrix2, CellInfo * const d_outputMatrix)
+{
+	int index = getGlobalIdx_1D_1D();
 	d_outputMatrix[index] = sumMatrix(d_inputMatrix1, d_inputMatrix2, index);
 }
 
-__global__ void matrixOperation(const float  * const d_inputMatrix1,
-	const float  * const d_inputMatrix2, float * const d_outputMatrix, float * const d_errorMap)
+__global__ void kernelPlusError(const CellInfo  * const d_inputMatrix1,
+	const CellInfo  * const d_inputMatrix2, CellInfo * const d_outputMatrix)
 {
-	int index = getGlobalIdx_2D_2D();
+	int index = getGlobalIdx_1D_1D();
 	d_outputMatrix[index] = sumMatrix(d_inputMatrix1, d_inputMatrix2, index);
-	injectError(d_outputMatrix, d_errorMap, getGlobalIdx_2D_2D());
+	if (isError(index))
+		injectError(d_outputMatrix[index]);
 }
 
-void fillErrorMap(float * const errorMap, const int numRows, const int numCols)
+
+
+//
+//void runKernel(dim3 gridSize, dim3 blockSize, float* in1, float* in2, float* out)
+//{
+//	cudaEvent_t start, stop; // Mam pewne obawy przed wyrzucaniem tego do osobnych funkcji, ¿eby nie zajmowa³o niepotrzebnie czasu systemowego
+//	float time;
+//	cudaEventCreate(&start);
+//	cudaEventCreate(&stop);
+//
+//	cudaEventRecord(start, 0);
+//
+//	matrixOperation <<< gridSize, blockSize >>>(in1, in2, out);
+//
+//	cudaEventRecord(stop, 0);
+//	cudaEventSynchronize(stop);
+//
+//	cudaEventElapsedTime(&time, start, stop);
+//	std::cout << "Time for the Kernel: " << time << std::endl;
+//}
+//
+//void runKernelWithError(dim3 gridSize, dim3 blockSize, float* in1, float* in2,
+//	float* out, float* error)
+//{
+//	cudaEvent_t start, stop;
+//	float time;
+//	cudaEventCreate(&start);
+//	cudaEventCreate(&stop);
+//
+//	cudaEventRecord(start, 0);
+//
+//	matrixOperation <<< gridSize, blockSize >>>(in1, in2, out, error);
+//
+//	cudaEventRecord(stop, 0);
+//	cudaEventSynchronize(stop);
+//
+//	cudaEventElapsedTime(&time, start, stop);
+//	std::cout << "Time for the ErrorKernel: " << time << std::endl;
+//}
+
+void testStartKernel()
 {
-	for (int i = 0; i < numRows*numCols; i++)
-	{
-		errorMap[i] = 10;
-	}
-}
+	Matrix m1("matrixes/bcsstk03.mtx");
+	Matrix m2("matrixes/bcsstk03.mtx");
 
-void runKernel(dim3 gridSize, dim3 blockSize, float* in1, float* in2, float* out)
-{
-	//std::cout << gridSize.x << gridSize.y << gridSize.z << "\n";
-	//std::cout << blockSize.x << blockSize.y << blockSize.z << "\n";
-	cudaEvent_t start, stop; // Mam pewne obawy przed wyrzucaniem tego do osobnych funkcji, ¿eby nie zajmowa³o niepotrzebnie czasu systemowego
-	float time;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
+	int arraySize = m1.getNonZeroValuesAmount();
+	int arrayBytes = arraySize * sizeof(CellInfo);
 
-	cudaEventRecord(start, 0);
+	// init CPU vars // no smart pointers in .cu allowed, watch out
+	CellInfo *host_mIn1 = new CellInfo[arraySize]; host_mIn1 = m1.getMatrix();
+	CellInfo *host_mIn2 = new CellInfo[arraySize]; host_mIn2 = m2.getMatrix();
+	CellInfo *host_mOut = new CellInfo[arraySize];
 
-	matrixOperation <<< gridSize, blockSize >>>(in1, in2, out);
+	// init GPU vars
+	CellInfo *device_mIn1;
+	CellInfo *device_mIn2;
+	CellInfo *device_mOut;
 
-	cudaEventRecord(stop, 0);
-	cudaEventSynchronize(stop);
+	// alloc GPU memory
+	gpuErrchk(cudaMalloc((void**)&device_mIn1, arrayBytes));
+	gpuErrchk(cudaMalloc((void**)&device_mIn2, arrayBytes));
+	gpuErrchk(cudaMalloc((void**)&device_mOut, arrayBytes));
+	// copy memory to device
+	gpuErrchk(cudaMemcpy(device_mIn1, host_mIn1, arrayBytes, cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(device_mIn2, host_mIn2, arrayBytes, cudaMemcpyHostToDevice));
 
-	cudaEventElapsedTime(&time, start, stop);
-	std::cout << "Time for the Kernel: " << time << std::endl;
-}
+	// launch kernel
+	kernel << <arraySize, 1 >> >(device_mIn1, device_mIn2, device_mOut);
+	// launch kernel with error
+	//kernelPlusError << <arraySize, 1 >> >(device_mIn1, device_mIn2, device_mOut);
+	gpuErrchk(cudaPeekAtLastError()); // debug
 
-void runKernelWithError(dim3 gridSize, dim3 blockSize, float* in1, float* in2,
-	float* out, float* error)
-{
-	cudaEvent_t start, stop;
-	float time;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
+	// copy memory from device
+	gpuErrchk(cudaMemcpy(host_mOut, device_mOut, arrayBytes, cudaMemcpyDeviceToHost));
+	
+	std::cout << "INPUT: \n";
+	for (int i = 0; i < arraySize; i++)
+		std::cout << host_mIn1[i].value << " ";
+	std::cout << "\nOUTPUT: \n";
+	for (int i = 0; i < arraySize; i++)
+		std::cout << host_mOut[i].value<< " ";
 
-	cudaEventRecord(start, 0);
-
-	matrixOperation <<< gridSize, blockSize >>>(in1, in2, out, error);
-
-	cudaEventRecord(stop, 0);
-	cudaEventSynchronize(stop);
-
-	cudaEventElapsedTime(&time, start, stop);
-	std::cout << "Time for the ErrorKernel: " << time << std::endl;
+	// cleaning
+	cudaFree(device_mIn1);
+	cudaFree(device_mIn2);
+	cudaFree(device_mOut);
+	delete[] host_mOut;
 }
--- src/invoker/kernel/kernel.cuh
+++ src/invoker/kernel/kernel.cuh
@@ -3,27 +3,33 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+struct CellInfo;
+
 // get threads global id
+__device__ int getGlobalIdx_1D_1D();
 __device__ int getGlobalIdx_2D_2D();
 
 // sum operation on two matrices
-__device__ float sumMatrix(const float * const d_inputMatrix1, const float * const d_inputMatrix2, int index);
+__device__ CellInfo sumMatrix(const CellInfo * const d_inputMatrix1, const CellInfo * const d_inputMatrix2, int index);
+
+// is there need for error?
+__device__ bool isError(int index);
 
 // injecting error into matrix
-__device__ void injectError(float * const d_inputMatrix, const float * const d_errorMap, int index);
+__device__ void injectError(CellInfo &inputCell);
 
 // kernel performing operations on two matrices ver without error injection
-__global__ void matrixOperation(const float  * const d_inputMatrix1,
-	const float  * const d_inputMatrix2, float * const d_outputMatrix);
+__global__ void kernel(const CellInfo  * const d_inputMatrix1,
+	const CellInfo  * const d_inputMatrix2, CellInfo * const d_outputMatrix);
 
 // kernel performing operations on two matrices ver with error injection
-__global__ void matrixOperation(const float  * const d_inputMatrix1,
-	const float  * const d_inputMatrix2, float * const d_outputMatrix, float * const d_errorMap);
-
-// fill error map with values (depracated)
-extern void fillErrorMap(float * const errorMap, const int numRows, const int numCols);
+__global__ void kernelPlusError(const CellInfo  * const d_inputMatrix1,
+	const CellInfo  * const d_inputMatrix2, CellInfo * const d_outputMatrix);
 
 // wrappers for C++ class calling kernel
-void runKernel(dim3 gridSize, dim3 blockSize, float* in1, float* in2, float* out);
-void runKernelWithError(dim3 gridSize, dim3 blockSize, float* in1, float* in2,
-	float* out, float* error);
+//void runKernel(dim3 gridSize, dim3 blockSize, float* in1, float* in2, float* out);
+//void runKernelWithError(dim3 gridSize, dim3 blockSize, float* in1, float* in2,
+//	float* out, float* error);
+
+
+void testStartKernel();
